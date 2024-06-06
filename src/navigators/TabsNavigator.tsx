347e712197fa9b8c755fd4ae3779afa61b4e8a43
AmosIvor <trantuanvu00000@gmail.com>
2024-06-06 21:46:59 +0700

    [TAB BOTTOM BAR] update tab

--- src/navigators/TabsNavigator.tsx
+++ src/navigators/TabsNavigator.tsx
@@ -1,88 +1,116 @@
+import { RowComponent, TextComponent } from '@components'
+import { appColors, appFonts } from '@constants'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import React from 'react'
-import CustomBottomTabs from '../components/CustomBottomTabs'
-
-import { HomeNavigator, OrderNavigator, StatisticNavigator } from '@navigators'
-import ChatScreen from '@screens/chats/ChatScreen'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { LogoutScreen } from '@screens/auths'
+import { ChatScreen } from '@screens/chats'
 import { HomeScreen } from '@screens/homes'
+import { OrderScreen } from '@screens/orders'
+import { StatisticScreen } from '@screens/statistics'
+import { Bag2, Chart21, Home2, LogoutCurve, Message } from 'iconsax-react-native'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
 type TabsStackParamList = {
-  Home: undefined
-  Order: undefined
-  Chat: undefined
-  Statistic: undefined
-  Logout: undefined
+  HomeTab: undefined
+  OrderTab: undefined
+  ChatTab: undefined
+  StatisticTab: undefined
+  LogoutTab: undefined
 }
 
 const TabsNavigator = () => {
-  const TabsStack = createBottomTabNavigator<TabsStackParamList>()
+  const Tab = createBottomTabNavigator<TabsStackParamList>()
 
   return (
-    <TabsStack.Navigator
-      screenOptions={{
-        tabBarShowLabel: false
-      }}
-      tabBar={(props) => <CustomBottomTabs {...props} />}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: appColors.White,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          height: 70,
+          justifyContent: 'center',
+          alignItems: 'center'
+        },
+
+        tabBarIcon: ({ focused, size, color }) => {
+          color = focused ? appColors.White : appColors.Black
+          size = focused ? 16 : 21
+          let icon = <Home2 variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+          let name = 'Home'
+          switch (route.name) {
+            case 'OrderTab':
+              icon = <Bag2 variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+              name = 'Order'
+              break
+
+            case 'ChatTab':
+              icon = <Message variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+              name = 'Chat'
+              break
+
+            case 'StatisticTab':
+              icon = <Chart21 variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+              name = 'Statistic'
+              break
+
+            case 'LogoutTab':
+              icon = <LogoutCurve variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+              name = 'Logout'
+              break
+
+            default:
+              icon = <Home2 variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+              name = 'Home'
+          }
+          return (
+            <RowComponent
+              styles={
+                focused
+                  ? {
+                      backgroundColor: appColors.statistic,
+                      height: 30,
+                      borderRadius: 100
+                    }
+                  : undefined
+              }
+            >
+              <View style={focused ? styles.iconContainer : undefined}>{icon}</View>
+              {focused && (
+                <TextComponent
+                  styles={{
+                    paddingHorizontal: 6,
+                    fontSize: 11,
+                    fontFamily: appFonts.medium
+                  }}
+                  text={name}
+                />
+              )}
+            </RowComponent>
+          )
+        }
+      })}
     >
-      <TabsStack.Screen
-        name='Home'
-        component={HomeScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon(props) {
-            return <MaterialCommunityIcons name='home' {...props} />
-          }
-        }}
-      />
-
-      <TabsStack.Screen
-        name='Order'
-        component={OrderNavigator}
-        options={{
-          headerShown: false,
-          tabBarIcon(props) {
-            return <MaterialCommunityIcons name='cart' {...props} />
-          }
-        }}
-      />
-
-      <TabsStack.Screen
-        name='Chat'
-        component={ChatScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon(props) {
-            return <MaterialCommunityIcons name='account' {...props} />
-          }
-        }}
-      />
-
-      <TabsStack.Screen
-        name='Statistic'
-        component={StatisticNavigator}
-        options={{
-          headerShown: false,
-          tabBarIcon(props) {
-            return <MaterialCommunityIcons name='account' {...props} />
-          }
-        }}
-      />
-
-      <TabsStack.Screen
-        name='Logout'
-        component={LogoutScreen}
-        options={{
-          headerShown: false,
-          tabBarIcon(props) {
-            return <MaterialCommunityIcons name='logout' {...props} />
-          }
-        }}
-      />
-    </TabsStack.Navigator>
+      <Tab.Screen name='HomeTab' component={HomeScreen} />
+      <Tab.Screen name='OrderTab' component={OrderScreen} />
+      <Tab.Screen name='ChatTab' component={ChatScreen} />
+      <Tab.Screen name='StatisticTab' component={StatisticScreen} />
+      <Tab.Screen name='LogoutTab' component={LogoutScreen} />
+    </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    width: 30,
+    height: 30,
+    backgroundColor: appColors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100
+  }
+})
 
 export default TabsNavigator
